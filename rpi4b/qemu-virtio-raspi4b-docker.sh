b8ff05c07fd50f695d50890847d01f1eca735157
Franck Dura <franck.dura.pro@protonmail.com>
2023-01-24 11:25:01 +0100

    changed bullseye to bookworm

--- rpi4b/qemu-virtio-raspi4b-docker.sh
+++ rpi4b/qemu-virtio-raspi4b-docker.sh
@@ -4,9 +4,9 @@ if  [[ $1 = "-b" ]]; then
     mkdir src/raspi4b-virtio
     apt-get update -y
     apt-get install -y wget unzip
-    wget https://raspi.debian.net/tested/20230102_raspi_4_bullseye.img.xz -P ./src/raspi4b-virtio
+    wget https://raspi.debian.net/tested/20230101_raspi_4_bookworm.img.xz -P ./src/raspi4b-virtio
     cd ./src/raspi4b-virtio
-    unxz 20230102_raspi_4_bullseye.img.xz
+    unxz 20230101_raspi_4_bookworm.img.xz
     IMAGE=`find ./ -name '2*.img'`
     losetup -f --show -P $IMAGE > loop.txt
     LOOPDIR=$(cat loop.txt)
@@ -28,4 +28,4 @@ else
     docker start raspi4bvirtio
 fi
 
-docker exec -it raspi4bvirtio /bin/bash -c "./qemu-6.2.0/build/aarch64-softmmu/qemu-system-aarch64 -m 4096 -M virt -cpu cortex-a72 -smp 4 -kernel ./boot4b/vmlinuz-5.10.0-20-arm64 -initrd ./boot4b/initrd.img-5.10.0-20-arm64 -drive file=./20230102_raspi_4_bullseye.img,if=none,format=raw,id=hdroot -append 'rw root=/dev/vda2 console=ttyAMA0 loglevel=8 rootwait fsck.repair=yes memtest=1' -device virtio-blk-device,drive=hdroot -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-device,netdev=net0 -no-reboot -nographic"
+docker exec -it raspi4bvirtio /bin/bash -c "./qemu-6.2.0/build/aarch64-softmmu/qemu-system-aarch64 -m 4096 -M virt -cpu cortex-a72 -smp 4 -kernel ./boot4b/vmlinuz-6.0.0-6-arm64 -initrd ./boot4b/initrd.img-6.0.0-6-arm64 -drive file=./20230101_raspi_4_bookworm.img,if=none,format=raw,id=hdroot -append 'rw root=/dev/vda2 console=ttyAMA0 loglevel=8 rootwait fsck.repair=yes memtest=1' -device virtio-blk-device,drive=hdroot -netdev user,id=net0,hostfwd=tcp::2222-:22 -device virtio-net-device,netdev=net0 -no-reboot -nographic"
